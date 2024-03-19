05bf0f3c68d535f839cdba5010bd43691efbb60f
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2024-03-19 15:23:11 +0900

    ci: cd to deploy to dev environment

---
+++ projects/admin/src/environments/environment.sample.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  environment: 'dev',
+  useEmulator: false,
+  blockchain: {
+    symbol: { network: 'testnet' },
+  },
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
+  },
+};
---
+++ projects/lp/src/environments/environment.sample.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  environment: 'dev',
+  useEmulator: true,
+  blockchain: {
+    symbol: { network: 'testnet' },
+  },
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
+  },
+};
